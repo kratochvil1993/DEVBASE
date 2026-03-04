eb28194eca970eadf2f3861f4da1c75a6a6fe312
Jiří Kratochvíl <kratochvil1993@gmail.com>
2026-03-04 14:49:49 +0100

    feat: fetch all note tags.

--- manage_notes.php
+++ manage_notes.php
@@ -13,6 +13,7 @@ $notes = getAllNotes($currentSort);
 $pinnedNotes = array_filter($notes, function($n) { return $n['is_pinned'] == 1; });
 $otherNotes = array_filter($notes, function($n) { return $n['is_pinned'] == 0; });
 $languages = getAllLanguages();
+$tags = getAllTags('note');
 $geminiApiKey = getSetting('gemini_api_key');
 $aiEnabled = getSetting('ai_enabled', '0') == '1' && (!empty($geminiApiKey) || !empty(getSetting('openai_api_key')));
 
