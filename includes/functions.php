aaba46b7dcc9c0f93a42d30f92941a1f0bba2c6e
Jiří Kratochvíl <kratochvil1993@gmail.com>
2026-03-05 09:20:07 +0100

    refactor: Update `db.php` require path to use `__DIR__` for improved resolution.

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'db.php';
+require_once __DIR__ . '/db.php';
 
 // Check if database and tables are created and up to date
 // Check if database and tables are created and up to date
