9ffa77bbbbec613d6eb47fc4ab1b569afb600d2a
Jiří Kratochvíl <kratochvil1993@gmail.com>
2026-03-13 08:14:29 +0100

    feat: Add Docker-specific database configuration to `config.example.php`.

--- includes/config.example.php
+++ includes/config.example.php
@@ -12,3 +12,11 @@ define('DB_HOST',   'localhost');      // host MySQL serveru (většinou 'localh
 define('DB_USER',   'your_db_user');   // MySQL uživatelské jméno
 define('DB_PASS',   'your_db_password'); // MySQL heslo
 define('DB_NAME',   'devbase');        // název databáze
+
+//docker build
+/*
+define('DB_HOST',   'mysql_db');  // název Docker service z docker-compose.yml
+define('DB_USER',   'root');
+define('DB_PASS',   'root');
+define('DB_NAME',   'devbase');
+*/
